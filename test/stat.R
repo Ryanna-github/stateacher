285e47f675d3bafd8f6c7919ad4d655942a7f1a4
‘renyan’ <1609532549@qq.com>
2021-09-10 14:31:31 +0800

    modify \n to keep format

--- test/stat.R
+++ test/stat.R
@@ -44,5 +44,5 @@ html_header="
 </head>
 <body>
 "
-write(paste(html_header, tb, sep=""), paste0("summary.html"))
+write(paste(html_header, tb, sep=""), file = paste0("summary.html"))
 print("Your summary.html file has been generated")
